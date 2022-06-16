239d89fef04ec0634216c9c6161f37369a35074a
Joel Bernerman <joel.bernerman@helsingborg.se>
2022-06-16 14:11:03 +0200

    ci: hax get it to build

--- build.php
+++ build.php
@@ -8,7 +8,6 @@ if (php_sapi_name() !== 'cli') {
 // Any command needed to run and build plugin assets when newly cheched out of repo.
 $buildCommands = [
     'npm install --no-progress',
-    'npm update @helsingborg-stad/styleguide',
     'npm run build',
     'mkdir -p ./assets/dist/data/ && cp ./node_modules/@helsingborg-stad/styleguide/assets/data/icons.json ./assets/dist/data/ico.json',
     'composer install --prefer-dist --no-progress --no-suggest'
